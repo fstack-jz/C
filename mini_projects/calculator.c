39e4e165d2e21b67fcf1f78a03612818075ece68
Cassian <fstack.jz@gmail.com>
2025-09-15 15:50:27 +0800

    Used switch instead of if statements in calculator

--- mini_projects/calculator.c
+++ mini_projects/calculator.c
@@ -2,76 +2,62 @@
 #include <string.h>
 #include <math.h>
 
+void getNumbers(float *a, float*b) {
+    printf("Enter first number: ");
+    scanf("%f", a);
+    printf("Enter second number: ");
+    scanf("%f", b);
+}
+
 
 int main() {
 
     float num1 = 0.0f;
     float num2 = 0.0f;
     float result = 0.0f;
-    char input[30] = "";
+    int input = 0;
 
     printf("Welcome to Calculator!!\n");
     
     printf("Choose operators:\n1) Add\n2) Subtraction\n3) Multiplication\n4) Division\n5) Exit.\n\n: ");
-    fgets(input, sizeof(input), stdin);
-    input[strlen(input) - 1] = '\0';
+    scanf(" %d", &input);
 
-    if (strcmp(input, "1") == 0) {
-
-        printf("\nEnter your first number: ");
-        scanf(" %f", &num1);
-
-        printf("Enter your second number: ");
-        scanf(" %f", &num2);
-
-        result = num1 + num2;
-
-        printf("\nThe result of %.f and %.f is: %.2f", num1, num2, result);
-    }
-    else if (strcmp(input, "2") == 0){
+    switch(input){
+        case 1:
+            getNumbers(&num1, &num2);
+            result = num1 + num2;
+            printf("\nThe result of %.f and %.f is : %.2f.\n", num1, num2, result);
+            break;
         
-        printf("\nEnter first number: ");
-        scanf(" %f", &num1);
+        case 2:
+            getNumbers(&num1, &num2);
+            result = num1 + num2;
+            printf("\nThe difference of %.f and %.f is: %.2f.\n", num1, num2, result);
+            break;
 
-        printf("Enter second number: ");
-        scanf(" %f", &num2);
+        case 3:
+            getNumbers(&num1, &num2);
+            result = num1 * num2;
+            printf("\nThe product of %.f and %.f is: %.2f.\n", num1, num2, result);
+            break;
 
-        result = num1 - num2;
-
-        printf("\nThe difference of %.f minus %.f is: %.2f", num1, num2, result);
-    }
-    else if (strcmp(input, "3") == 0) {
+        case 4:
+            getNumbers(&num1, &num2);
+            result = num1 / num2;
+            printf("\nThe qoutient of %.f and %.f is: %.2f.\n", num1, num2, result);
+            break;
         
-        printf("\nEnter first number: ");
-        scanf(" %f", &num1);
+        case 5:
+            printf("Goodbye :(.");
+            break;
 
-        printf("Enter second number: ");
-        scanf(" %f", &num2);
+        default:
+            printf("Please only select between 1-5!");
+            break;
 
-        result = num1 * num2;
-
-        printf("\nThe product of %.f times %.f is: %.2f", num1, num2, result);
+            
     }
-    else if (strcmp(input, "4") == 0) {
-        printf("\nEnter first number: ");
-        scanf(" %f", &num1);
-        
 
-        printf("Enter second number: ");
-        scanf(" %f", &num2);
-
-        result = num1 / num2;
-
-        printf("\nThe qoutient of %.f divided by %.f is: %.2f", num1, num2, result);
-    }
-    else if (strcmp(input, "5") == 0) {
-
-        printf("\nGoodbye! :(");
-        return 0;
-    }
-    else {
-        printf("\nError: Please input valid number!");
-    }
 
     return 0;
 }
